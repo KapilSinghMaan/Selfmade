3d6777e7bac7f6626f08be696aad0aade121688e
KapilSinghMaan <166300100+KapilSinghMaan@users.noreply.github.com>
2024-05-15 20:28:21 +0530

    Add files via upload

---
+++ MountainPattern.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    system("CLS");
+    int n,i,j,k=0;
+    printf("Enter limit : ");
+    scanf("%d",&n);
+    for ( i = 1; i <=n; i++)
+    {
+        printf("|");
+        if (i%4==0)
+        {
+            printf("**");
+            k++;
+        }
+        else
+        {
+            for ( j = 1; j <=(i-(4*k)); j++)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    
+    getch();
+}
